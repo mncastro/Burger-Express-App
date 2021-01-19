6d1f4087d525c6934bec3f71309754aa14ec0e98
¨Mario Castro¨ <mnc634@utexas.edu>
2021-01-19 17:57:32 -0600

    fixed buttons and functions

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,6 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-devoured").on("click", function(event) {
+  $(".change-notdevoured").on("click", function(event) {
     var id = $(this).data("id");
     var newDevour = $(this).data("newdevour");
 
@@ -14,7 +14,7 @@ $(function() {
       data: newDevourState
     }).then(
       function() {
-        console.log("changed not eaten to", newDevour);
+        console.log("changed devoured to", newDevour);
         // Reload the page to get the updated list
         location.reload();
       }
@@ -26,7 +26,7 @@ $(function() {
     event.preventDefault();
 
     var newBurger = {
-      name: $("#burger").val().trim(),
+      burger_name: $("#burger").val().trim(),
       devoured: $("[name=devoured]:checked").val().trim()
     };
 
